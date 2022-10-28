90c15cb178cc0ea487136991a873f29bb8b6fe1d
Gustavo de Assis <gustavo.siqueira.421@gmail.com>
2022-10-28 11:23:21 -0300

    criação dos componentes e rotas

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GlobalStyle from "../assets/styles/GlobalStyle"
+
+import Login from "./login/Login"
+import Signup from "./login/Signup"
+import Subscriptions from "./subscriptions/Subscriptions"
+import MainPage from "./home/MainPage"
+
+
+export default function App(){
+    return(<>
+    <BrowserRouter>
+        <GlobalStyle/>
+        <Routes>
+            <Route path="/" element={<Login/>}/>
+            <Route path="/sign-up" element={<Signup/>}/>
+            <Route path="/subscriptions/:ID_DO_PLANO" element={<Subscriptions/>}/>
+            <Route path="/home" element={<MainPage/>}/>
+        </Routes>
+    </BrowserRouter>
+    </>)
+}
